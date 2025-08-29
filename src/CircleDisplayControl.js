ff4666d2b5db568eed30d1cfe5a657c7610748b1
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 13:38:31 +0100

    Update CircleDisplayControl.js
    extends to circle 7

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -54,10 +54,10 @@ function circleTextControls(){
     document.getElementById("descText").style.display = data.textTriggers[0]?`flex`:`none`
     document.getElementById("descText").innerText = `Circle 2 (${formatWhole(data.numbers[1])}%) provides a ${format(effect)}x multiplier to the speed of Circle 1.`
     document.getElementById("descText2").style.display = data.textTriggers[1]?`block`:`none`
-    document.getElementById("descText2").innerHTML = `All circles above Circle 2 multiply the effect of the previous Circle.<br><span style="font-size: 0.9rem">Hover for more info.</span>`
+    document.getElementById("descText2").innerHTML = `All circles above Circle 2 multiply the effect of the previous Circle.<br><span style="font-size: 0.9rem">Hover to see precise values.</span>`
 
     DOM(`descText3`).innerText = makeCircleEffectText()
 
-    DOM(`descText4`).style.display = data.numbers.length > 5 ? `block` : `none`
-    document.getElementById("descText4").innerText = `Circle 6 (${formatWhole(data.numbers[5])}%) increases the minimum percent of all lower Circles by ${format(minimumNumber(0).sub(1))}%`
+    DOM(`descText4`).style.display = data.numbers.length > 6 ? `block` : `none`
+    document.getElementById("descText4").innerText = `Circle 7 (${formatWhole(data.numbers[6])}%) increases the minimum percent of all lower Circles by ${format(minimumNumber(0).sub(1))}%`
 }
