eed8ccd05e71a339ecdeb2bacb4af1bdf446865c
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 17:04:46 +0100

    Update CircleDisplayControl.js

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -17,7 +17,7 @@ function createBars(su=false){
         let newBar = document.createElement('div')
         let prevBar = document.getElementById(`bar${progressBars.length-1}`) // i have to do lots of meaningless notes so the browser updates each time - smaller changes don't affect it 
         newBar.classList.add('circular-progress') // it's such a pain
-        newBar.id = `bar${progressBars.length}`
+        newBar.id = `bar${progressBars.length}` // // i have to do lots of meaningless notes so the browser updates each time - smaller changes don't affect it 
         newBar.style.height = `${180+(progressBars.length)*20}px`// this bit is cursed
         newBar.style.width = `${180+(progressBars.length)*20}px` // so is this
         container.appendChild(newBar)
