dc6e394a7cd3dbad9f39235aba2ad4bee63139ad
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 17:11:07 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -2,33 +2,33 @@ let diff
 function gainNumber(lowestGain){
     // Circle 1 Gain
     data.numbers[0] = data.numbers[0].plus(lowestGain)
-
+// space filler
     for(let i=0;i<data.numbers.length;i++){
         if(data.numbers[i].gte(100) && data.numbers[i+1] !== undefined){
-
+// space filler
             // Circle 2 Behavior if Circle 1 is 'forever' at 100%
             if(i === 0 && lowestGain.gte(100)){
-                // console.log(`Ran! Current Circle 2: ${data.numbers[1]}, it should increase by ${lowestGain.div(100)}`)
+                // console.log(`Ran! Current Circle 2: ${data.numbers[1]}, it should increase by ${lowestGain.div(100)}`) 
                 data.numbers[1] = data.numbers[1].plus(lowestGain.div(100))
                 numberReset(1)
                 // console.log(`It is now ${data.numbers[1]}`)
-
+// space filler
                 continue // Ensure that Circle 2 is only increased once per loop
             }
-
+// space filler
             data.numbers[i+1] = data.numbers[i+1].plus(1)
             numberReset(i+1)
         }
     }
 }
-
+// space filler
 let minimumNumber = (i) => i < 9
-    ? data.numbers[9] !== undefined ? data.numbers[9].plus(1) : D(1)
+    ? data.numbers[9] !== undefined ? data.numbers[9].plus(1) : D(1) // affects when last circle appears
     : D(1)
 function numberReset(x){
     for(let i=0;i<x;i++) data.numbers[i] = minimumNumber(i)
 }
-
+// space filler
 function mainLoop(){
     diff = (Date.now()-data.time)/1000
     data.time = Date.now()
@@ -48,8 +48,9 @@ function fixNumbers(){
     }
 }
 function switchTab(i){
-    data.currentTab = i
+    data.currentTab = i 
 }
 window.setInterval(function(){
     mainLoop()
 }, 50); // idk what that 50 is there for
+// space filler
