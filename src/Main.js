9b7c2799e278f414400a93463d10dcf7fdc166e0
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 15:33:02 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -4,12 +4,12 @@ function gainNumber(lowestGain){
     data.numbers[0] = data.numbers[0].plus(lowestGain)
 
     for(let i=0;i<data.numbers.length;i++){
-        if(data.numbers[i].gte(10) && data.numbers[i+1] !== undefined){
+        if(data.numbers[i].gte(2) && data.numbers[i+1] !== undefined){
 
             // Circle 2 Behavior if Circle 1 is 'forever' at 100%
-            if(i === 0 && lowestGain.gte(10)){
+            if(i === 0 && lowestGain.gte(2)){
                 // console.log(`Ran! Current Circle 2: ${data.numbers[1]}, it should increase by ${lowestGain.div(100)}`)
-                data.numbers[1] = data.numbers[1].plus(lowestGain.div(10))
+                data.numbers[1] = data.numbers[1].plus(lowestGain.div(2))
                 numberReset(1)
                 // console.log(`It is now ${data.numbers[1]}`)
 
@@ -22,8 +22,8 @@ function gainNumber(lowestGain){
     }
 }
 
-let minimumNumber = (i) => i < 6
-    ? data.numbers[6] !== undefined ? data.numbers[6].plus(1) : D(1)
+let minimumNumber = (i) => i < 19
+    ? data.numbers[19] !== undefined ? data.numbers[19].plus(1) : D(1)
     : D(1)
 function numberReset(x){
     for(let i=0;i<x;i++) data.numbers[i] = minimumNumber(i)
