49a0aadc095076147a8846874161f31ef90b6caf
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 15:33:54 +0100

    Update CircleDisplayControl.js

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -6,7 +6,7 @@ let progressValues = [D(0)];
 function progress(i, x){
     let hexAdds = [126+(i*(25+(i*5))), i*(25+(i*5)), 180-(i*(15+(i*5)))]
     progressValues[i]=x;
-    valueContainer.textContent = `${format(progressValues[0])}/10`
+    valueContainer.textContent = `${format(progressValues[0])}/2`
     progressBars[i].style.background = `conic-gradient(
       rgb(${hexAdds[0]}, ${hexAdds[1]}, ${hexAdds[2]}) ${progressValues[i].times(180)}deg,
       #000 ${progressValues[i].times(180)}deg
