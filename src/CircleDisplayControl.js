dd0b87593cdb32b02358184a0bc7594ff3c3492a
shrub47381 <whatsapp37890@gmail.com>
2025-08-29 15:14:52 +0100

    Update CircleDisplayControl.js

--- src/CircleDisplayControl.js
+++ src/CircleDisplayControl.js
@@ -6,7 +6,7 @@ let progressValues = [D(0)];
 function progress(i, x){
     let hexAdds = [126+(i*(25+(i*5))), i*(25+(i*5)), 180-(i*(15+(i*5)))]
     progressValues[i]=x;
-    valueContainer.textContent = `${format(progressValues[0])}`
+    valueContainer.textContent = `${format(progressValues[0])}/10`
     progressBars[i].style.background = `conic-gradient(
       rgb(${hexAdds[0]}, ${hexAdds[1]}, ${hexAdds[2]}) ${progressValues[i].times(36)}deg,
       #000 ${progressValues[i].times(36)}deg
@@ -18,8 +18,8 @@ function createBars(su=false){
         let prevBar = document.getElementById(`bar${progressBars.length-1}`)
         newBar.classList.add('circular-progress')
         newBar.id = `bar${progressBars.length}`
-        newBar.style.height = `${180+(progressBars.length)}px`
-        newBar.style.width = `${180+(progressBars.length)}px`
+        newBar.style.height = `${180+(progressBars.length*10)}px`
+        newBar.style.width = `${180+(progressBars.length*10)}px`
         container.appendChild(newBar)
         newBar.appendChild(prevBar)
         if (!su){
@@ -45,19 +45,19 @@ function setupBars(x){
 function makeCircleEffectText(){
     let string = ""
     for (let i = 2; i < data.numbers.length; i++) {
-        string += `Circle ${i+1} (${formatWhole(data.numbers[i])}): ${format(higherEffects[i-2])}x to the Circle ${i} effect.\n`
+        string += `Circle ${i+1} (${formatWhole(data.numbers[i])}/10): ${format(higherEffects[i-2])}x to the Circle ${i} effect.\n`
     }
     return string
 }
 
 function circleTextControls(){
     document.getElementById("descText").style.display = data.textTriggers[0]?`flex`:`none`
-    document.getElementById("descText").innerText = `Circle 2 (${formatWhole(data.numbers[1])}/10) provides a ${format(effect)}x multiplier to the speed of Circle 1.`
+    document.getElementById("descText").innerText = `Circle 2 (${formatWhole(data.numbers[1])}%) provides a ${format(effect)}x multiplier to the speed of Circle 1.`
     document.getElementById("descText2").style.display = data.textTriggers[1]?`block`:`none`
     document.getElementById("descText2").innerHTML = `All circles above Circle 2 multiply the effect of the previous Circle.<br><span style="font-size: 0.9rem">Hover to see precise values.</span>`
 
     DOM(`descText3`).innerText = makeCircleEffectText()
 
     DOM(`descText4`).style.display = data.numbers.length > 6 ? `block` : `none`
-    document.getElementById("descText4").innerText = `Circle 7 (${formatWhole(data.numbers[6])}/10) increases the minimum percent of all lower Circles by ${format(minimumNumber(0).sub(1)*(10))}%`
+    document.getElementById("descText4").innerText = `Circle 7 (${formatWhole(data.numbers[6])}/10) increases the minimum percent of all lower Circles by ${format(minimumNumber(0).sub(1))*10}%`
 }
