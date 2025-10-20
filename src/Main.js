166945b4b19fc6e4226a3589fe6af582dfb51f35
shrub <shrub47381@gmail.com>
2025-10-20 17:01:28 +0100

    game speed bugfix

--- src/Main.js
+++ src/Main.js
@@ -30,7 +30,7 @@ function numberReset(x){
 }
 // space filler
 function mainLoop(){
-    diff = (Date.now()-data.time)/(1000/data.numbers[0])
+    diff = (Date.now()-data.time)/(1000/(data.numbers[0]+1))
     data.time = Date.now()
     calcLoop()
     for(let i=0;i<data.numbers.length;i++) progress(i, data.numbers[i])
