93d7a038ca4f3a2a2eba0db41edccc6e457cd098
shrub <shrub47381@gmail.com>
2025-10-23 16:52:29 +0100

    bugfix10

--- src/Circles.js
+++ src/Circles.js
@@ -3,12 +3,12 @@ let higherEffects = Array(100).fill(D(1))
 
 function calcCircleEffects(){
     if(data.numbers[1]!==undefined){
-        effect = (data.numbers[1]).times(higherEffects[0])
+        effect = (data.numbers[1].sqrt).times(higherEffects[0])
     }
 
     let i=0
     while(data.numbers[i+2]!==undefined){
-        higherEffects[i] = (((data.numbers[i+2].plus(1))).times((higherEffects[i+1])))
+        higherEffects[i] = ((data.numbers[i+2].plus(1).sqrt()).times(higherEffects[i+1])
         i++
     }
 }
